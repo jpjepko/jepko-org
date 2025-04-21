477ed28bd52a0e61056f547903339fd44094cbe1
John Paul Jepko <jpjepko@umich.edu>
2025-04-21 18:11:55 -0400

    start header (not quite right yet but good enough)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,57 @@
 import './App.css'
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Jeppy</h1>
+      <div className="header">
+        <div className="header-top-border">
+          <a href="#" className="top-link"><span className="top-link-first-char">h</span>ome</a>
+          <a href="#" className="top-link"><span className="top-link-first-char">a</span>bout</a>
+        </div>
+        <div className="header-content">
+          <span>&gt; Lorem Ipsum</span>
+        </div>
+      </div>
+
+      <div className="content">
+        { /* placeholder to test floating hdr */ }
+        <h1>Header</h1>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <h1>Header</h1>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <h1>Header</h1>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <h1>Header</h1>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+        <p>Paragraph</p>
+        <p>Bruh</p>
+      </div>
     </>
   )
 }
