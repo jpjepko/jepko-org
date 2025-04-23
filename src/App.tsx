d9ddab259ae52829dce491177c056636cec5eb61
John Paul Jepko <jpjepko@umich.edu>
2025-04-23 15:24:44 -0400

    sticky header (TODO: links scroll too far)

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,46 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className="grid grid-cols-4 font-mono text-center text-xl bg-slate-950 text-green-500">
-        <div>home</div>
-        <div>about</div>
-        <div>projects</div>
-        <div>resume</div>
-        <div className="col-span-4 text-5xl">&gt; John Paul Jepko</div>
-      </div>
-      
-      <div className="text-white">
-        { /* placeholder to test floating hdr */ }
-        <h1>Header</h1>
-        <p>Paragraph</p>
-      </div>
+      <header className="font-mono text-center text-xl bg-slate-950 text-green-500 p-2 border border-green-500 rounded-md sticky top-0">
+        <nav className="grid grid-cols-4 text-xl">
+          <a href="#home" className="text-green-500 hover:text-green-600">home</a>
+          <a href="#about" className="text-green-500 hover:text-green-600">about</a>
+          <a href="#projects" className="text-green-500 hover:text-green-600">projects</a>
+          <a href="#resume" className="text-green-500 hover:text-green-600">resum&eacute;</a>
+        </nav>
+        <h1 className="col-span-4 text-6xl pt-4">&gt; John Paul Jepko</h1>
+      </header>
+
+      <main>
+        <div className="font-mono text-center text-xl bg-slate-950 text-white mx-15 p-2 border border-green-500 rounded-md">
+          <h1 id="about" className="text-center text-4xl text-green-500">About</h1>
+          <p>This is a paragraph</p>
+        </div>
+        <div className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
+          <h1 id="about" className="text-center text-4xl text-green-500">About</h1>
+        </div>
+        <div className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
+          <h1 id="about" className="text-center text-4xl text-green-500">About</h1>
+        </div>
+        <div className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
+          <h1 id="about" className="text-center text-4xl text-green-500">About</h1>
+        </div>
+        <div className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
+          <h1 id="about" className="text-center text-4xl text-green-500">About</h1>
+        </div>
+        <div className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
+          <h1 id="about" className="text-center text-4xl text-green-500">About</h1>
+        </div>
+        <div className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
+          <h1 id="about" className="text-center text-4xl text-green-500">About</h1>
+        </div>
+        <div className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
+          <h1 id="about" className="text-center text-4xl text-green-500">About</h1>
+        </div>
+        <div className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
+          <h1 id="about" className="text-center text-4xl text-green-500">About</h1>
+        </div>
+      </main>
     </>
   )
 }
