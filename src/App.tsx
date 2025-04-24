bf055c4ca98cbb3e246398d18305144bad850842
John Paul Jepko <jpjepko@umich.edu>
2025-04-24 03:07:14 -0400

    replace home with skills

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,14 @@ function App() {
     <>
       <header className="font-mono text-center text-xl bg-slate-950 text-green-500 p-2 border border-green-500 rounded-md sticky top-0">
         <nav className="grid grid-cols-4 text-xl">
-          <a href="#home" className="text-green-500 hover:text-green-600">home</a>
           <a href="#about" className="text-green-500 hover:text-green-600">about</a>
           <a href="#projects" className="text-green-500 hover:text-green-600">projects</a>
+          <a href="#skills" className="text-green-500 hover:text-green-600">skills</a>
           <a href="#resume" className="text-green-500 hover:text-green-600">resum&eacute;</a>
         </nav>
         <h1 className="col-span-4 text-6xl pt-4">&gt; John Paul Jepko</h1>
       </header>
       <main>
-        <div id="home" className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
-          <h1 className="text-center text-4xl text-green-500">Home</h1>
-          <p>Welcome to my portfolio!</p>
-        </div>
         <div id="about" className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
           <h1 className="text-center text-4xl text-green-500">About</h1>
           <p>This is a paragraph about me.</p>
@@ -25,25 +21,9 @@ function App() {
           <h1 className="text-center text-4xl text-green-500">Projects</h1>
           <p>Details about my projects.</p>
         </div>
-        <div id="resume" className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
-          <h1 className="text-center text-4xl text-green-500">Resumé</h1>
-          <p>Details about my resumé.</p>
-        </div>
-        <div id="resume" className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
-          <h1 className="text-center text-4xl text-green-500">Resumé</h1>
-          <p>Details about my resumé.</p>
-        </div>
-        <div id="resume" className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
-          <h1 className="text-center text-4xl text-green-500">Resumé</h1>
-          <p>Details about my resumé.</p>
-        </div>
-        <div id="resume" className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
-          <h1 className="text-center text-4xl text-green-500">Resumé</h1>
-          <p>Details about my resumé.</p>
-        </div>
-        <div id="resume" className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
-          <h1 className="text-center text-4xl text-green-500">Resumé</h1>
-          <p>Details about my resumé.</p>
+        <div id="skills" className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
+          <h1 className="text-center text-4xl text-green-500">Skills</h1>
+          <p>Welcome to my skills!</p>
         </div>
         <div id="resume" className="font-mono text-center text-xl bg-slate-950 text-white m-3 p-2 border border-green-500 rounded-md">
           <h1 className="text-center text-4xl text-green-500">Resumé</h1>
