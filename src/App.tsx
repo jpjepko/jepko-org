d1078f8192c8f8c2a4d3b87336d7d46af7902d0c
John Paul Jepko <jpjepko@umich.edu>
2025-04-25 03:22:10 -0400

    change first letter color of nav links

--- src/App.tsx
+++ src/App.tsx
@@ -29,10 +29,10 @@ function App() {
     <>
       <header className="font-mono text-center text-xl bg-slate-950 text-green-500 p-2 border border-green-500 rounded-md sticky top-0">
         <nav className="grid grid-cols-4 text-xl">
-          <a href="#about" className="text-green-500 hover:text-green-600">about</a>
-          <a href="#projects" className="text-green-500 hover:text-green-600">projects</a>
-          <a href="#skills" className="text-green-500 hover:text-green-600">skills</a>
-          <a href="#resume" className="text-green-500 hover:text-green-600">resum&eacute;</a>
+          <a href="#about" className="group text-green-500 hover:text-green-600"><span className="underline font-bold text-red-500 group-hover:text-red-600">a</span>bout</a>
+          <a href="#projects" className="group text-green-500 hover:text-green-600"><span className="underline font-bold text-red-500 group-hover:text-red-600">p</span>rojects</a>
+          <a href="#skills" className="group text-green-500 hover:text-green-600"><span className="underline font-bold text-red-500 group-hover:text-red-600">s</span>kills</a>
+          <a href="#resume" className="group text-green-500 hover:text-green-600"><span className="underline font-bold text-red-500 group-hover:text-red-600">r</span>esum&eacute;</a>
         </nav>
         <h1 className="col-span-4 text-6xl pt-4">&gt; John Paul Jepko</h1>
       </header>
